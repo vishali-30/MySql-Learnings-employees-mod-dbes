eecd09014a8d3091ddb67d2643b3a97ff2421dfa
vishali-30 <116253142+vishali-30@users.noreply.github.com>
2024-01-23 09:52:38 +0530

    Update triggers.sql

--- MySql-Emplyoees-db/triggers.sql
+++ MySql-Emplyoees-db/triggers.sql
@@ -1,46 +1,43 @@
+-- Switch to the employees_db database --
 USE employees_db;
 
-
--- trigger to hash user password while inserting a new record -- 
+-- Trigger to hash user password while inserting a new record --
 DELIMITER &&
 CREATE TRIGGER HashPassword
 BEFORE INSERT ON employees
 FOR EACH ROW
 BEGIN
   SET NEW.password = SHA2(NEW.password, 256);
-END&&
+END &&
 DELIMITER ;
 
---  START TRANSACTION;  
+-- Uncomment the lines below if you want to use transactions and rollback features --
+-- START TRANSACTION;
+-- COMMIT;
+-- SET autocommit = 0;
+-- SAVEPOINT employee_insertion;
+-- ROLLBACK TO employee_insertion;
+-- DELETE FROM mobile_no;
 
--- COMMIT; 
---  SET autocommit = 0;
---  SAVEPOINT employee_insertion; 
---  ROLLBACK TO employee_insertion; 
---  
---  DELETE FROM mobile_no;  
- 
- 
- -- tigger to insert hire date automatically -- 
- DELIMITER &&
+-- Trigger to insert hire date automatically --
+DELIMITER &&
 CREATE TRIGGER insert_hire_date 
 BEFORE INSERT ON employees
- FOR EACH ROW
+FOR EACH ROW
 BEGIN
     SET NEW.hire_date = NOW();
-END&&
+END &&
 DELIMITER ;
 
--- procedure to verify the user email and password using the hashed password  --
-DELIMITER &&  
-create procedure employee_login(IN email_ids VARCHAR(150), IN  passwords VARCHAR(100))
-BEGIN   
- SELECT * FROM employees
- WHERE email_id = email_ids
-  AND password = SHA2(passwords, 256);
-END && 
-DELIMITER ;  
+-- Procedure to verify the user email and password using the hashed password --
+DELIMITER &&
+CREATE PROCEDURE employee_login(IN email_ids VARCHAR(150), IN passwords VARCHAR(100))
+BEGIN
+    SELECT * FROM employees
+    WHERE email_id = email_ids
+    AND password = SHA2(passwords, 256);
+END &&
+DELIMITER ;
 
--- calling the procedure -- 
-call employee_login ( 'forpractice@gmail.com','thisIsAPassword');
-  
+-- Calling the procedure --
+CALL employee_login('forpractice@gmail.com', 'thisIsAPassword');
